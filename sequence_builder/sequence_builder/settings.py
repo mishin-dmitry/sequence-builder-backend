64dd4d17180451da4534e2357b11dae0926c0040
d.mishin <d.mishin@rambler-co.ru>
2023-09-26 07:42:22 +0300

    fixed installed apps

--- sequence_builder/sequence_builder/settings.py
+++ sequence_builder/sequence_builder/settings.py
@@ -42,8 +42,8 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "django_filters",
-    "sequence_builder.sequence_builder.api.apps.ApiConfig",
-    "sequence_builder.sequence_builder.asana.apps.AsanaConfig",
+    "sequence_builder.api.apps.ApiConfig",
+    "sequence_builder.asana.apps.AsanaConfig",
     "corsheaders",
 ]
 
